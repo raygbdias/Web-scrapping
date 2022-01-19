8811ea194287de456fe2574f2b550f1deb8c4eea
raygbdias <raygabrielsantana@gmail.com>
2022-01-19 10:36:32 +0000

    indexacao de paginas

--- web-tecmundo.py
+++ web-tecmundo.py
@@ -6,9 +6,15 @@ url = urlopen("https://www.tecmundo.com.br/busca?q=jogos")
 bs = BeautifulSoup(url, 'html.parser')
 
 dados = bs.find_all('h2', {'class':'tec--card__title'})
-print (dados)
+#print (dados)
 for i in dados:
-    filhas = i.findChildren("a")
-    print(filhas[0])
-  #  print(filhas[1])
-  #  print(filhas[2])
+    nomes = i.findChildren("a")
+    print(nomes[0].text)
+    link = i.findChildren("a")
+    print(link[0]["href"] )
+    
+    url = urlopen(link[0]["href"])
+    bs = BeautifulSoup(url, 'html.parser')
+    data = bs.find_all('div', {'class':'tec--article__body-grid'})
+    print (data)
+    
